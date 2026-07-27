832d241445ef16c3520ed744388bdd8bdab17d63
制杖 <ygsgdbd@hotmail.com>
2026-07-27 11:27:32 +0800

    chore: use dedicated V2Bar Sparkle key

--- Project.swift
+++ Project.swift
@@ -5,7 +5,7 @@ import ProjectDescription
 let appVersion = Environment.appVersion.getString(default: "0.2.0")
 let buildVersion = Environment.buildVersion.getString(default: "0")
 let sparklePublicKey = ProcessInfo.processInfo.environment["TUIST_SPARKLE_PUBLIC_KEY"]
-    ?? "iQt5X/HXFWmvB7pKxm7mxHZPXh2FG9UA8fFPmAKcE8I="
+    ?? "PXKEkd6QKqpXltmUKy233T80qTb8iIfjvOwHThSH6vI="
 
 // 基础依赖
 let baseDependencies: [TargetDependency] = [
